5f92724a199886d476a0ac5012bf51208561419f
kv.streltsov <kv.streltsov@yandex.ru>
2023-09-13 17:10:21 +0500

    end day

--- src/modules/super-admin/use-cases/updatePostByBlogIdSqlUseCase.ts
+++ src/modules/super-admin/use-cases/updatePostByBlogIdSqlUseCase.ts
@@ -2,12 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostInBlogDto } from '../../posts/dto/create-post.dto';
 import { BlogsQuerySqlRepository } from '../../blogs/repositories/postgresql/blogs.query.sql.repository';
 import { PostsSqlRepository } from '../../posts/repositories/postgresql/posts.sql.repository';
+import { PostsQuerySqlRepository } from '../../posts/repositories/postgresql/posts.query.sql.repository';
 
 @Injectable()
 export class UpdatePostByBlogIdSqlUseCase {
   constructor(
     private blogsQueryRepository: BlogsQuerySqlRepository,
     private postsRepository: PostsSqlRepository,
+    private postsQuerySqlRepository: PostsQuerySqlRepository,
   ) {}
   async execute(
     blogId: string,
@@ -16,6 +18,9 @@ export class UpdatePostByBlogIdSqlUseCase {
   ) {
     const foundBlog = await this.blogsQueryRepository.getBlogById(blogId);
     if (!foundBlog) throw new NotFoundException();
+
+    const foundPost = await this.postsQuerySqlRepository.getPostById(postId);
+    if (!foundPost) throw new NotFoundException();
 
     const updatedPost = await this.postsRepository.updatePost(
       postId,
